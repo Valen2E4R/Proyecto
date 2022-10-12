0fd0c163446c732b091cb1ebae5e265d738a2a0e
Valen2E4R <103860033+Valen2E4R@users.noreply.github.com>
2022-10-12 13:10:48 -0300

    Ejercicio
    ejercicio tema 4

--- Main.java
+++ Main.java
@@ -1,28 +1,21 @@
 public class Main {
-
     public static void main(String[] args) {
-        int param1 = 30;
-        int param2 = 20;
-        int param3 = 40;
-
-        var valor = suma(param1, param2, param3);
-
-        System.out.println(valor);
-        coche micoche = new coche();
-        micoche.AgregarPuertas();
-        System.out.println(micoche.puertas);
-    }
-    public static int suma(int a, int b, int c) {
-        return a + b + c;
-    }
-}
-
-class coche {
-    public int puertas = 4;
-
-    public void AgregarPuertas() {
-        this.puertas++;
+        var estacion = "invierno";
+        switch (estacion) {
+            case "verano":
+                System.out.println("es verano");
+                break;
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            case "otoño":
+                System.out.println("es otoño");
+                break;
+            default:
+                System.out.println("no es ninguna estacion");
+        }
     }
 }
-
-
